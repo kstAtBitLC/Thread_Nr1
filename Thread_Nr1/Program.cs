5f5ae5c8a9d4eef693c4a6d7df1e82e835fc3aa3
kstAtBitLC <klaus.stolz@bitlc.de>
2020-03-03 10:44:32 +0100

    Add project files.

---
+++ Thread_Nr1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Nr1 {
+    class Program {
+        static void Main ( string [] args ) {
+            
+            //Thread 1
+            ThreadStart thstart = new ThreadStart (Hallo);
+            Thread meinThread = new Thread (thstart);
+            
+            meinThread.Start ();
+            meinThread.Join ();
+            
+
+            // Thead 2
+            ParameterizedThreadStart pts = new ParameterizedThreadStart (Hallo);
+            Thread meinZweiterThread = new Thread (pts);
+            meinZweiterThread.Start ("Hallo Welt");
+            meinZweiterThread.Join ();
+
+            Console.WriteLine ("Hi");
+            Console.Read ();
+        }
+
+        // Klassenmethode
+        private static void Hallo () {
+            for ( int i = 0 ; i < 10 ; i++ ) {
+                Console.WriteLine ( "wichtige thread-Information" );
+            }
+        }
+        // Klassenmethode
+        private static void Hallo (object o) {
+            for ( int i = 0 ; i < 10 ; i++ ) {
+                Console.WriteLine ( "total wichtige thread-Information {0}", (string) o );
+            }
+        }
+    }
+}
---
+++ Thread_Nr1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ( "Thread_Nr1" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "Thread_Nr1" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2020" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible ( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ( "1b5ccd5d-92f8-42cc-9d4f-1fc8e8e21cf6" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
